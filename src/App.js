5ae5b365a4ceb6f3c633fd8d319249518dc13d45
Naa-A <61292016+Naa-A@users.noreply.github.com>
2024-05-23 23:17:16 +0000

    base html frame added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Ashikai and is
-        <a
-          href="http://https://github.com/Naa-A/she_codes_wk_five"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Ashikai and is
+          <br />
+          <a
+            href="http://https://github.com/Naa-A/she_codes_wk_five"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
