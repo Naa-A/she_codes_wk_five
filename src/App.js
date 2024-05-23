238663b42d84df01b3791aa18eb84a538edd34e3
Naa-A <61292016+Naa-A@users.noreply.github.com>
2024-05-23 22:36:20 +0000

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Ashikai and is
+        <a
+          href="http://https://github.com/Naa-A/she_codes_wk_five"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
